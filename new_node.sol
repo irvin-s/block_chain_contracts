11a64240b28dc04ef56738d7859915d8c2be4232
Irvin <61360847+irvin-s@users.noreply.github.com>
2021-06-04 14:53:11 -0300

    Updating Contracts

--- new_node.sol
+++ new_node.sol
@@ -2,4 +2,17 @@ pragma solidity >=0.5.0 <0.6.0
 
 contract NewNode {
     
+    struct nodeData {
+        uint cnpj = 14;
+        string nameAgen;
+        uint cnpj;
+        string nameNode;
+        string ipNode;
+    }
+
+    nodeData[] public node;
+
+    function addNode(string memory _nAgen, uint _cnpj, string memory _nNode, string memory _ipNode) private {
+        nodeData(node.push(_nAgen, _cnpj, _nNode, _ipNode))
+    }
 }
--- new_user.sol
+++ new_user.sol
@@ -2,4 +2,19 @@ pragma solidity >=0.5.0 <0.6.0
 
 contract NewUser {
     
+    struc UserData {
+        uint cnpjDigits = 14;
+        uint cpfDigits = 11;
+        string nameAgen;
+        uint cnpj;
+        string nameMan;
+        string nameUsr;
+        uint cpf;
+    }
+
+    UserData[] public users;
+
+    function addUser(string memory _nAgen, uint _cnpj, string memory _nMan, string memory _nUsr, uint _cpf) private {
+        user.push(users.push(_nAgen, _cnpj, _nMan, _nUsr, _cpf));
+    }
 }
